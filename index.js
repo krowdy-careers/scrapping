56fce15fa558654fd77c52aa4fad226742f97ddc
Anderson Sinche <andersonsinche@MacBook-Pro.local>
2024-11-07 20:57:15 -0500

    init project

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
 const puppeteer = require("puppeteer");
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+    args: [
+      "--start-maximized", // you can also use '--start-fullscreen'
+    ],
+  });
   const page = await browser.newPage();
-  await page.goto("https://www.occ.com.mx/empleos/");
+  await page.goto("https://api.regulaforensics.com/?utm_source=docs");
+
+ 
+  const elementHandle = await page.waitForSelector("input[type=file]");
+  await elementHandle.uploadFile("./images/dni.jpeg");
+
+
+ /*
+  await page.waitForSelector("tbody>tr");
+
+  const dniInformation = await page.evaluate(() => {
+    let rowElements = document.querySelectorAll("tbody>tr");
+    rowElements = [...rowElements];
+
+    const dniInformation = rowElements.map((el) => {
+      const [
+        { innerText: attribute },
+        { innerText: MRZ },
+        { innerText: visualZone },
+      ] = el.children;
+      return { attribute, MRZ, visualZone };
+    });
+
+    return dniInformation;
+  });
+
+  console.log(dniInformation);
+  */
 })();
